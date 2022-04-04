08b8958a41d89c92ed7740ee898ba4fa1f386411
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-04-04 20:17:40 +0600

    header navbar complete

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Reviews from './components/Reviews/Reviews'
 import Dashboard from './components/Dashboard/Dashboard';
 import Blogs from './components/Blogs/Blogs';
 import About from './components/About/About';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my Plant Paradise</h1>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
         <Route path='/reviews' element={<Reviews />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
         <Route path='/blogs' element={<Blogs />}></Route>
