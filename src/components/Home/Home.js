19f52c2e74a9a7cd26e159ebfdd2d82430abbe20
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-04-04 21:32:10 +0600

    bootstrap and font awesome included

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLeaf } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is from home</h1>
+        <div className='top-banner'>
+            <div className="banner-left">
+                <h1 className='display-2'><span className='green'>Plant<FontAwesomeIcon icon={faLeaf} /></span>Paradise</h1>
+            </div>
+            <div className="banner-right">
+                <h1>Hello from right part</h1>
+            </div>
         </div>
     );
 };
