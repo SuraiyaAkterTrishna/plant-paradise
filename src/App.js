5c6d1a399a6573666141beda578534c9f8b2d06b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-04-04 19:25:49 +0600

    create all components and set route in them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews'
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Welcome to my Plant Paradise</h1>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>This is from Reviews</h1>
+        </div>
+    );
+};
+
+export default Reviews;
