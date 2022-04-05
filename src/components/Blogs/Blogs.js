7f03e64c37e4b3f552b428640f2df95c1f3f2226
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-04-05 17:01:15 +0600

    blogs area add style

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <div>
+        <div className = "container my-5">
+            <div className='bg-primary rounded p-4 text-white my-5'>
                 <p>1. What is Context API? What is the purpose of Context API?</p>
                 <p>- To send data from one component to another child component in React, it has to be sent as props. When data has to be sent from the main component to a component at a much lower level than the child component, the props have to be drilled to capture each component and then the data has to be sent to the lower component. The context API is used to fix this problem. Data can be passed directly from the main component to any component via the context API.</p>
             </div>
-            <div>
+            <div className='bg-success rounded p-4 text-white my-5'>
                 <p>2. What is Semantic tag?</p>
                 <p>- Semantic tags clearly describe their meaning in a human and machine readable way. Semantic tag helps browsers and developers understand what will actually that element contains. By adding symantic tag in our code, we will provide more additional information about our code, that will make easy to understand the developer and browser what the meaning of the page and its content is, it will be very helpful to update and modify the code in future. Example - {`<header>, <footer> , <aside> , <nav> , <article>`} etc.</p>
             </div>
-            <div>
+            <div className='bg-danger rounded p-4 text-white my-5'>
                 <p>3. What are the difference between Inline and block elements?</p>
                 <p>- The elements that cover the entire space of the parent element is called block elements. Block elements always start with a new line and they take whole space of that line. Example of block elements - {`<div> , <ul> , <h1> - <h6> , <article> , <address>`}  etc. 
 
